dc711b4304890ec3910ceb0edf4dc3e898676b59
Mr.  Phil <mr.phil1@gmx.net>
2022-01-09 08:47:24 +0100

    update to version 2.2.3

--- docker-compose.sh
+++ docker-compose.sh
@@ -3,7 +3,7 @@
 apps="curl"
 osName="$(uname -s)"
 osArch="$(uname -m)"
-dockerComposeVersion="v2.1.0"
+dockerComposeVersion="v2.2.3"
 dockerComposeGithub="https://github.com/docker/compose/releases/download/${dockerComposeVersion}/docker-compose-${osName}-${osArch}"
 #########################
 #this is the auto-install routine
